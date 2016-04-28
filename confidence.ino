d74641d596ffba6788fe3b8313e379656ac9d463
Jordan Shaw <jrdnshw@gmail.com>
2016-04-28 13:39:54 -0400

    The motor now steps by two between  1/2 rotations to control the scanning behaviour

--- confidence.ino
+++ confidence.ino
@@ -2,21 +2,36 @@
 // Example NewPing library sketch that does a ping about 20 times per second.
 // ---------------------------------------------------------------------------
 
-#include <NewPing.h>
+#define DEBUG true
 
+#include <NewPing.h>
+#include <Array.h>
+
+#include <AccelStepper.h>
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
 #define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
-#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+#define MAX_DISTANCE 400 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
-const long interval = 100;
+// Controls Installation Mode
+// 0 = scanning
+// 1 = go to furthest distance
+int motorStep = 2;
+
+const long interval = 1000;
 unsigned long previousMillis = 0;
 
 int currentDistance = 0;
 int previousDistance = 0;
+
+int motorStep = 2;
+int motorDirection = 0;
+int motorStartPosition = 0;
+int motorCurrentPosition = 0;
+int motorMaxPosition = 26;
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
@@ -25,7 +40,14 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
 // Connect a stepper motor with 50 steps per revolution (7.2 degree)
 // to motor port #2 (M3 and M4)
-Adafruit_StepperMotor *myMotor = AFMS.getStepper(100, 1);
+Adafruit_StepperMotor *myMotor = AFMS.getStepper(50, 1);
+//AccelStepper stepper1(forwardstep1, backwardstep1);
+
+// Setting up the different values for the sonar values while rotating
+const byte size = 14;
+int rawArray[size] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
+int sensorArrayValue[size];
+Array<int> array = Array<int>(sensorArrayValue, size);
 
 void setup() {
   // Open serial monitor at 115200 baud to see ping results.
@@ -44,25 +66,73 @@ void loop() {
   
   if(currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
+    
+    if (DEBUG == true){
+      
+      // Debug / Prototype Motor
+      currentDistance = random(5, 400); 
 
-    currentDistance = sonar.ping_cm();
-     
-    if(currentDistance != 0){
       Serial.print("Ping: ");
       Serial.print(currentDistance); // Send ping, get distance in cm and print result (0 = outside set distance range)
       Serial.println("cm");
+
+      int currentStep = motorCurrentPosition / 2;
+      sensorArrayValue[currentStep] = currentDistance;
+      
+    } else {
+      
+      // Gets the sonar distance
+      currentDistance = sonar.ping_cm();
+      
+      if(currentDistance != 0){
+        Serial.print("Ping: ");
+        Serial.print(currentDistance); // Send ping, get distance in cm and print result (0 = outside set distance range)
+        Serial.println("cm");
+  
+        int currentStep = motorCurrentPosition / 2;
+        sensorArrayValue[currentStep] = currentDistance;
+      }
+      
+    }
+
+    //  Controls the motor
+    if(motorCurrentPosition <= motorMaxPosition && motorDirection == 0){
+      myMotor->step(motorStep, FORWARD, MICROSTEP); 
+      motorCurrentPosition += motorStep;
+    } else if (motorCurrentPosition >= motorStartPosition && motorDirection == 1) {
+      myMotor->step(motorStep, BACKWARD, MICROSTEP); 
+      motorCurrentPosition -= motorStep;
+    }
+
+    Serial.print("motorCurrentPosition: ");
+    Serial.println(motorCurrentPosition);
+
+    Serial.print("motorDirection: ");
+    Serial.println(motorDirection);
+
+    // Updates the direction of the motor
+    if (motorCurrentPosition <= 0){
+      // forwards
+      motorDirection = 0;
+    } else if (motorCurrentPosition >= 25) {
+      // backwards
+      motorDirection = 1;
     }
 
     previousDistance = currentDistance;
+    
   }
 
-  Serial.println("Interleave coil steps");
-  myMotor->step(100, FORWARD, MICROSTEP); 
-  myMotor->step(100, BACKWARD, MICROSTEP); 
-
-  
-//  delay(100);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
-//  Serial.print("Ping: ");
-//  Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
-//  Serial.println("cm");
 }
+
+// you can change these to DOUBLE or INTERLEAVE or MICROSTEP!
+// wrappers for the first motor!
+void forwardstep1() {  
+  myMotor->onestep(FORWARD, MICROSTEP);
+}
+
+void backwardstep1() {  
+  myMotor->onestep(BACKWARD, MICROSTEP);
+}
+
+
